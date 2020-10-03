8d00f4fe771f1bc795b975a572d486b4c53c1163
Tseing <im.yczeng@foxmail.com>
2020-10-03 18:41:43 +0800

    sweep

---
+++ sweep/rtl/dds.v
@@ -0,0 +1,25 @@
+module dds(
+    input               clk_wave,
+    input               clk_freq,
+    input               sys_rst,
+    output  reg [13:0]  addr
+);
+
+reg [13:0]  freq_cons;
+reg [8:0]   freq_cnt;
+
+always @(posedge clk_wave) begin
+        freq_cons <= {5'b00000, freq_cnt[8:0]};
+        addr <= addr + freq_cons;           //1kHz~300kHz
+end
+
+always @(posedge clk_freq or negedge sys_rst) begin
+    if(!sys_rst)
+        freq_cnt <= 9'd0;
+    else if(freq_cnt < 9'd300)
+        freq_cnt <= freq_cnt + 9'd1;
+    else
+        freq_cnt <= 9'd0;
+end
+
+endmodule
---
+++ sweep/rtl/top.v
@@ -0,0 +1,42 @@
+module top(
+    input               sys_clk,
+    input               sys_rst_n,
+    output              da_clk,
+    output              da_wrt,
+    output      [13:0]  da_data
+);
+
+wire            clk_125m;
+wire            clk_wave;
+wire            clk_freq;
+wire    [13:0]  data;
+wire    [13:0]  addr;
+
+
+assign da_data = data;
+assign da_clk = ~clk_125m;
+assign da_wrt = clk_125m;
+
+
+pll u_pll(
+    .areset     (~sys_rst_n),
+    .inclk0     (sys_clk),
+    .c0         (clk_125m),
+    .c1         (clk_wave),
+    .c2         (clk_freq)
+);
+
+rom u_rom(
+    .clock      (sys_clk),
+    .address    (addr),
+    .q          (data)
+);
+
+dds u_dds(
+    .clk_wave   (clk_wave),
+    .clk_freq   (clk_freq),
+    .sys_rst    (sys_rst_n),
+    .addr       (addr)
+);
+
+endmodule
